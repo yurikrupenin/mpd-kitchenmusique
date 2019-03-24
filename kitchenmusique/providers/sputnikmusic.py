c6a1a8cd7173509ad08f51e4954b5fd3f254d3e7
Yuri Krupenin <yuri.krupenin@gmail.com>
2019-03-24 21:30:33 +0300

    Fixed error that made SputnikMusic produce incorrect AlbumEntries

--- kitchenmusique/providers/sputnikmusic.py
+++ kitchenmusique/providers/sputnikmusic.py
@@ -11,18 +11,20 @@ class SputnikMusic:
         page = requests.get("https://sputnikmusic.com")
         self.tree = html.fromstring(page.content)
 
+        albums = []
+
         for pos in range(1, 16):
             # This lazy Xpath grabs (artist album) tuples from "Trending albums" table.
             # This will probably break after slightest redesign, but hey it works for now.
-            self.albumlist.append(AlbumEntry(\
-                 self.tree.xpath("/html/body/table/tr[3]/td/table/tr[1]/td[3]/table/tr[2]/td/table/tr[{0}]/td[1]/a/font/text()".format(pos)),
-                 self.tree.xpath("/html/body/table/tr[3]/td/table/tr[1]/td[3]/table/tr[2]/td/table/tr[{0}]/td[1]/a/font/span/text()".format(pos))
-                ))
+            albums.append(AlbumEntry(
+                 self.tree.xpath("/html/body/table/tr[3]/td/table/tr[1]/td[3]/table/tr[2]/td/table/tr[{0}]/td[1]/a/font/text()".format(pos))[0],
+                 self.tree.xpath("/html/body/table/tr[3]/td/table/tr[1]/td[3]/table/tr[2]/td/table/tr[{0}]/td[1]/a/font/span/text()".format(pos))[0]
+            ))
 
-        self.albumlist = list(filter(\
+        self.albumlist = list(filter(
             lambda entry:
-                len(entry.artist) > 0 and len(entry.album) > 0,\
-            self.albumlist))
+                len(entry.artist) > 0 and len(entry.album) > 0,
+            albums))
                
 
     # Returns list of albums in "Trending albums" category
